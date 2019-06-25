5785ba789839eb871e6d57c0e3818c557c452732
Naveen <navindogiparthi@gmail.com>
2019-06-25 12:07:12 +0530

    Initial Commit

---
+++ Data-Passing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data-Passing
+//
+//  Created by Krish on 25/06/19.
+//  Copyright © 2019 Krish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
